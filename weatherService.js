24afed9902e8d6534ee2afb38dcfcf486b7b9504
User <User@WRSD-LD309-026.dut.ac.za>
2025-01-24 09:49:36 +0200

    axios, weather api and weatherService.js added

---
+++ weatherService.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const API_KEY = "bbee9f377aee90fd5323f04fb473db6e"
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+
+export const getWaether = async (lat, lon) => {
+    try {
+        const response = await axios.get(BASE_URL, {
+            params: {
+                lat,
+                lon,
+                appid: API_KEY,
+                units:"metric"
+            }
+        })
+        return response.data
+    } catch (error) {
+        console.error("Error fetching weather data", error)
+    }
+}
